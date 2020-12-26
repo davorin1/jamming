7fa80a3f20c854a4dcf0cee65f98d6ee1c1eb78a
Mekic <mekic@MacBook-Air-od-Mekic.local>
2020-12-27 00:08:53 +0100

    added TrackList to PlayList

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,7 +4,27 @@ import SearchBar from "../SearchBar/SearchBar";
 import SearchResults from "../SearchResults/SearchResults";
 import Playlist from "../Playlist/Playlist";
 
+
+
 class App extends React.Component {
+  constructor(props) {
+      super(props);
+
+      this.state = {
+          searchResults: [
+              { name: 'name1', artist: 'artist1', album: 'album1', id: 1},
+              { name: 'name2', artist: 'artist2', album: 'album2', id: 2}
+              ],
+          playlistName: 'Nova lista',
+          playlistTracks: [
+              { name: 'pjesma1', artist: 'artist1', album: 'album1', id: 1},
+              { name: 'pjesma2', artist: 'artist2', album: 'album2', id: 2}
+          ]
+      };
+  }
+
+
+
   render() {
     return (
         <div>
@@ -12,8 +32,8 @@ class App extends React.Component {
           <div className="App">
             <SearchBar />
             <div className="App-playlist">
-              <SearchResults />
-             <Playlist />
+              <SearchResults searchResults={this.state.searchResults} />
+             <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} />
             </div>
           </div>
         </div>
