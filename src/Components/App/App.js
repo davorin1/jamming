0a2beb36a990ea12f41a46b9b1b98f3b2bb7b0f2
Mekic <mekic@MacBook-Air-od-Mekic.local>
2020-12-27 18:37:57 +0100

    remove tracks from playlist

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -24,6 +24,7 @@ class App extends React.Component {
       };
 
       this.addTrack = this.addTrack.bind(this);
+      this.removeTrack = this.removeTrack.bind(this);
   }
 
     addTrack(track) {
@@ -37,6 +38,17 @@ class App extends React.Component {
         })
     }
 
+    removeTrack(track) {
+        let tracks = this.state.playlistTracks;
+
+        tracks = tracks.filter(currentTrack => currentTrack.id !== track.id);
+
+        this.setState({
+            playlistTracks: tracks
+        })
+
+    }
+
 
 
   render() {
@@ -47,7 +59,7 @@ class App extends React.Component {
             <SearchBar />
             <div className="App-playlist">
               <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
-             <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} />
+             <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} />
             </div>
           </div>
         </div>
